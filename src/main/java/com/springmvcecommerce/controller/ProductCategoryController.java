8ae211cb8d20a02bbb60a778d0b75ba897eea464
OuniAbir <ouniabir118@gmail.com>
2020-12-28 15:31:51 +0100

    Add CrossOrgin support

--- src/main/java/com/springmvcecommerce/controller/ProductCategoryController.java
+++ src/main/java/com/springmvcecommerce/controller/ProductCategoryController.java
@@ -3,6 +3,7 @@ package com.springmvcecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springmvcecommerce.dao.ProductCategoryDAO;
 import com.springmvcecommerce.entity.ProductCategory;
 
+@CrossOrigin(origins="http://localhost:4200")  
 @RestController
 public class ProductCategoryController {
 	
