3d897c8884f95d7446e5a6c5acd7cc4c8107f5b6
OuniAbir <ouniabir118@gmail.com>
2020-12-31 15:16:04 +0100

    Solve the application context root bug

--- src/main/java/com/springmvcecommerce/controller/ProductCategoryController.java
+++ src/main/java/com/springmvcecommerce/controller/ProductCategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springmvcecommerce.dao.ProductCategoryDAO;
@@ -13,6 +14,7 @@ import com.springmvcecommerce.entity.ProductCategory;
 
 @CrossOrigin(origins="http://localhost:4200")  
 @RestController
+@RequestMapping("/api")
 public class ProductCategoryController {
 	
 	@Autowired
